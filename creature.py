4ab258a8a8a38383e0829be9d7ef839154509f9d
Sandro <31536101+sandromund@users.noreply.github.com>
2022-01-27 01:24:40 +0100

    added states and hunting

--- creature.py
+++ creature.py
@@ -2,7 +2,7 @@ import random
 
 
 class Creature:
-    def __init__(self, x, y, size, color, dis_width, dis_height):
+    def __init__(self, x, y, size, color, dis_width, dis_height, vision, speed):
         self.x = x
         self.y = y
         self.size = size
@@ -10,15 +10,52 @@ class Creature:
         self.dis_width = dis_width
         self.dis_height = dis_height
         self.last_move = random.randrange(0, 4)
+        self.is_alive = True
+        self.vision = vision
+        self.speed = speed
+
+        self.state = 0
+        self.target = None
 
     def move(self):
+        if self.state == 0:
+            self.random_move()
+        if self.state == 1:
+            self.follow()
+
+    def follow(self):
+        x, y = self.target
+        if self.x < x:
+            if self.speed > x - self.x:
+                self.x = x
+            else:
+                self.x += self.speed
+        if self.x > x:
+            if self.speed > self.x - x:
+                self.x = x
+            else:
+                self.x -= self.speed
+        if self.y < y:
+            if self.speed > y - self.y:
+                self.y = y
+            else:
+                self.y += self.speed
+        if self.y > y:
+            if self.speed > self.y - y:
+                self.y = y
+            else:
+                self.y -= self.speed
+
+
+
+    def random_move(self):
         if random.randrange(0, 101) < 95:
             r = self.last_move
         else:
             r = random.randrange(0, 4)
             self.last_move = r
         move = 0
-        speed = 1   # self.size
+        speed = self.speed
         if r == 0:  # LEFT
             if self.x - self.size > 0:
                 self.x -= speed + move
--- game.py
+++ game.py
@@ -2,16 +2,23 @@ import pygame
 from creature import Creature
 import random
 import time
+import math
 
 
-dis_width = 1000
-dis_height = 1000
+def overlap(x1, y1, r1, x2, y2, r2):
+    # return r2 >= math.sqrt((x1 - x2)**2 + (y1 - y2)**2) +r1
+    return (r1 - r2)**2 >= (x1 - x2)**2 + (y1 - y2)**2
+
+
+dis_width = 1600
+dis_height = 1200
 
 
 creature = []
-for i in range(20):
+for i in range(30):
     r, g, b = random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)
-    c = Creature(random.randrange(0, dis_width), random.randrange(0, dis_height), 10, (r, g, b), dis_width, dis_height)
+    c = Creature(random.randrange(0, dis_width), random.randrange(0, dis_height), random.randrange(5, 11),
+                 (r, g, b), dis_width, dis_height, random.randrange(50, 101), random.randrange(1, 6))
     creature.append(c)
 
 pygame.init()
@@ -22,7 +29,7 @@ red = (255, 0, 0)
 clock = pygame.time.Clock()
 
 
-dis = pygame.display.set_mode((dis_width, dis_width))
+dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Sandro ist cool.')
 
 
@@ -32,12 +39,35 @@ while not game_over:
         if event.type == pygame.QUIT:
             game_over = True
 
+    for c1 in creature:
+        c1.state = 0
+        c1.target = None
+        for c2 in creature:
+            if c1 != c2 and c1.is_alive and c2.is_alive:
+                # check if creatures meet
+                # bigger creatures eat smaller ones
+                if overlap(c1.x, c1.y, c1.size, c2.x, c2.y, c2.size):
+                    if c1.size > c2.size:
+                        c1.size += c2.size // 3
+                        c2.is_alive = False
+                if overlap(c1.x, c1.y, c1.vision, c2.x, c2.y, c2.size):
+                    if c1.size > c2.size:
+                        c1.target = (c2.x, c2.y)
+                        c1.state = 1 # the hunt begin
+
+
+
+
     dis.fill(white)
     for c in creature:
-        pygame.draw.circle(dis, c.color, (c.x, c.y), c.size)
-        pygame.draw.circle(dis, 0, (c.x, c.y), c.size*8, 1)
+        if c.is_alive:
+            pygame.draw.circle(dis, c.color, (c.x, c.y), c.size)
+            pygame.draw.circle(dis, 0, (c.x, c.y), c.vision, 1)
+        if c.target:
+            pygame.draw.line(dis, c.color, (c.x, c.y), (c.target[0], c.target[1] ))
     for c in creature:
-        c.move()
+        if c.is_alive:
+            c.move()
     pygame.time.wait(50)
     pygame.display.update()
 
