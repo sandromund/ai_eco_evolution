6a571dd49ed88a87f536a5783a5ffc4d48803679
Sandro <31536101+sandromund@users.noreply.github.com>
2023-10-21 17:28:14 +0200

    Bug atm

--- voronoi/main.py
+++ voronoi/main.py
@@ -4,20 +4,20 @@ import numpy as np
 
 import voronoi as vn
 
-n = 100
-window_size = 1000
+n = 10
+window_size = 100
 width, height = window_size, window_size
 background_color = (0, 0, 0)
-n_points = 100
 
 pygame.init()
 
 screen = pygame.display.set_mode((window_size, window_size))
 pygame.display.set_caption('Voronoi')
 
-points = np.random.randint(low=0, high=window_size, size=(n, 2))  # TODO
+points = np.random.randint(low=0, high=window_size-1, size=(n, 2))  # TODO
 colors = np.random.randint(low=0, high=255, size=(n, 3))
 fild = np.full((width, height, 3), 0, dtype=int)
+fild = vn.cal_coloring(points, width, height, colors, fild)
 
 while True:
     for event in pygame.event.get():
@@ -25,6 +25,7 @@ while True:
             pygame.quit()
             sys.exit()
     screen.fill(background_color)
+
     pygame.surfarray.blit_array(screen, fild)
 
     vn.draw_points(points, screen)
--- voronoi/voronoi.py
+++ voronoi/voronoi.py
@@ -1,5 +1,5 @@
-import numpy as np
 import pygame as pg
+from numba import njit
 
 
 def draw_points(points, screen, radius=3):
@@ -7,3 +7,16 @@ def draw_points(points, screen, radius=3):
         pg.draw.circle(screen, (255, 255, 255), points[i], radius)
 
 
+@njit(fastmath=True)
+def cal_coloring(points, width, height, colors, fild):
+    for x in range(width):
+        for y in range(height):
+            min_dist = None
+            for i in range(len(points)):
+                d = (((x - points[i][0]) ** 2 + (y - points[i][1]) ** 2) ** 0.5)
+                if min_dist is None or d < min_dist:
+                    if x == y == 0:
+                        print(points[i], colors[i], d)
+                    fild[x, y] = colors[i]
+    print(fild.shape)
+    return fild
