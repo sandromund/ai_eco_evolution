ab54601b3d33a3647331318955d7a7d2a00c871d
Sandro <31536101+sandromund@users.noreply.github.com>
2023-10-21 16:32:43 +0200

    Update main.py

--- voronoi/main.py
+++ voronoi/main.py
@@ -1,8 +1,10 @@
 import pygame
 import sys
+import numpy as np
 
 import voronoi as vn
 
+n = 100
 window_size = 1000
 width, height = window_size, window_size
 background_color = (0, 0, 0)
@@ -13,9 +15,9 @@ pygame.init()
 screen = pygame.display.set_mode((window_size, window_size))
 pygame.display.set_caption('Voronoi')
 
-
-
-
+points = np.random.randint(low=0, high=window_size, size=(n, 2))  # TODO
+colors = np.random.randint(low=0, high=255, size=(n, 3))
+fild = np.full((width, height, 3), 0, dtype=int)
 
 while True:
     for event in pygame.event.get():
@@ -23,5 +25,7 @@ while True:
             pygame.quit()
             sys.exit()
     screen.fill(background_color)
+    pygame.surfarray.blit_array(screen, fild)
+
     vn.draw_points(points, screen)
     pygame.display.flip()
